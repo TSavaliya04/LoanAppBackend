3fd022080cfd5c2841b1e735347d0731a97b095e
Tushar Savaliya <tusharsavaliya4444@gmail.com>
2025-10-01 23:15:02 +0530

    bug fixes

--- LoanPortal.API/src/LoanPortal.Core/Services/UserService.cs
+++ LoanPortal.API/src/LoanPortal.Core/Services/UserService.cs
@@ -221,7 +221,10 @@ namespace LoanPortal.Core.Services
             {
                 string token = "sp=racwdli&st=2025-10-01T17:14:14Z&se=2026-10-02T01:29:14Z&sv=2024-11-04&sr=c&sig=tbTUGvn1%2F7uCyUtIvk8coOlzS9RD%2FGKBtdNyVxLR33Q%3D"; 
                 var user = await _userRepository.GetUserById(_loginUserDetails.UserID);
-                user.Profile = user.Profile + "?" + token;
+                if (string.IsNullOrEmpty(user.Profile)){
+                    user.Profile = user.Profile + "?" + token;
+                }
+
                 if (user == null)
                 {
                     throw new ValidationException($"User not found.");

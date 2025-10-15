c68bec5d7be05f5f9e836a445f22716ae6cc0965
Tushar Savaliya <tusharsavaliya4444@gmail.com>
2025-10-15 16:00:52 +0530

    Change in quickquote api

--- LoanPortal.API/src/LoanPortal.Core/Services/PreApprovalService.cs
+++ LoanPortal.API/src/LoanPortal.Core/Services/PreApprovalService.cs
@@ -459,9 +459,11 @@ public class PreApprovalService : IPreApprovalService
         quote.HazardInsurance = preApproval.PurchaseInfo.HazardInsurance.Value;
         quote.MortgageInsurance = preApproval.PurchaseInfo.MiPercent.Value;
         quote.HoaFee = preApproval.PurchaseInfo.AssociationFee.Value;
-        quote.MonthlyTotal = (decimal)(quote.PrincipalAndInterest + quote.PropertyTax + quote.PropertyTax + quote.HazardInsurance + quote.MortgageInsurance + quote.MortgageInsurance + quote.HoaFee);
+        quote.MonthlyTotal = (decimal)(quote.PrincipalAndInterest + quote.PropertyTax + quote.HazardInsurance + quote.MortgageInsurance + quote.HoaFee);
 
-        quote.ClosingCosts = (GetEstClosingCost(preApproval,new FHAReport())).TotalEstSettlementCharges;
+        //quote.ClosingCosts = (GetEstClosingCost(preApproval,new FHAReport())).TotalEstSettlementCharges;
+        LenderFeesDTO lenderFees = preApproval.LenderFees;
+        quote.ClosingCosts = (decimal)(lenderFees.LoanOriginationFee + lenderFees.DiscountFee + lenderFees.UpfrontMip + lenderFees.AppraisalFee + lenderFees.EscrowFees + lenderFees.TitleFees + lenderFees.ThirdPartyLenderFee);
         quote.DownPayment = downAmount;
 
         PrepaidItemsDTO prePaid = preApproval.PrepaidItems;

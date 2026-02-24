2dc5586fcb78797fb96d794e0ced70dd1512ecfa
TSavaliya04 <160272252+TSavaliya04@users.noreply.github.com>
2026-02-24 20:17:01 +0530

    Delete LoanPortal.API directory

--- LoanPortal.API/tests/LoanPortal.Tests/Models/ApiResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace LoanPortal.Tests.Models
-{
-    public class ApiResponse<T>
-    {
-        public T Data { get; set; }
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; }
-    }
-} 

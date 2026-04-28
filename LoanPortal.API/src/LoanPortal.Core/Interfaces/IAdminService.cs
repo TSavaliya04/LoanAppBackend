ff91d59e47349ddcaa030a18fca8550b9ce76e8e
TSavaliya04 <160272252+TSavaliya04@users.noreply.github.com>
2026-04-28 13:23:49 +0530

    company filter specific changes

--- LoanPortal.API/src/LoanPortal.Core/Interfaces/IAdminService.cs
+++ LoanPortal.API/src/LoanPortal.Core/Interfaces/IAdminService.cs
@@ -4,7 +4,7 @@ namespace LoanPortal.Core.Interfaces
 {
     public interface IAdminService
     {
-        Task<AdminDashboardDTO> GetAdminDashboard(DateTime startDate, DateTime endDate);
+        Task<AdminDashboardDTO> GetAdminDashboard(DateTime startDate, DateTime endDate, Guid? companyId = null);
         Task<PagedAgentsDTO> GetUsers(DefaultRequest request);
         Task<PagedRecentQuotesDTO> GetRecentQuotes(RecentQuoteRequest request);
         Task<QuotesOverviewDTO> GetQuotesOverview(DateTime startDate, DateTime endDate, Guid userId);

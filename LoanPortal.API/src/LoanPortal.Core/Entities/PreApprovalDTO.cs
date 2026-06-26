977246866d58107c4948a6b4ea7262f050afc150
Tushar Savaliya <tusharsavaliya4444@gmail.com>
2026-06-26 20:23:00 +0530

    added 4 fields to borrowerinfo

--- LoanPortal.API/src/LoanPortal.Core/Entities/PreApprovalDTO.cs
+++ LoanPortal.API/src/LoanPortal.Core/Entities/PreApprovalDTO.cs
@@ -1,5 +1,6 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using LoanPortal.Shared.Enum;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,6 +37,18 @@ namespace LoanPortal.Core.Entities
 
         [BsonElement("borrowerEmail")]
         public string? BorrowerEmail { get; set; }
+
+        [BsonElement("dateOfBirth")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [BsonElement("currentAddress")]
+        public string? CurrentAddress { get; set; }
+
+        [BsonElement("ssn")]
+        public string? SSN { get; set; }
+
+        [BsonElement("maritalStatus")]
+        public MaritalStatus? MaritalStatus { get; set; }
 
         [BsonElement("createdAt")]
         public DateTime? CreatedAt { get; set; }
--- LoanPortal.API/src/LoanPortal.Shared/Enum/Enums.cs
+++ LoanPortal.API/src/LoanPortal.Shared/Enum/Enums.cs
@@ -70,4 +70,13 @@ namespace LoanPortal.Shared.Enum
         Purchase = 0,
         Refinance = 1
     }
+
+    public enum MaritalStatus
+    {
+        Single = 0,
+        Married = 1,
+        Divorced = 2,
+        Widowed = 3,
+        Separated = 4
+    }
 }

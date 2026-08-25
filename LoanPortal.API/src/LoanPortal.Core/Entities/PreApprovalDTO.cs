198c08add32248680b3cc41dc1d092c1099b815c
Tushar Savaliya <tusharsavaliya4444@gmail.com>
2026-08-25 16:38:15 +0530

    Added fields  for HELOC and HELOAN and restructure DTOs

--- LoanPortal.API/src/LoanPortal.Core/Entities/PreApprovalDTO.cs
+++ LoanPortal.API/src/LoanPortal.Core/Entities/PreApprovalDTO.cs
@@ -170,24 +170,6 @@ namespace LoanPortal.Core.Entities
         [BsonElement("currentLoanBalance")]
         public decimal CurrentLoanBalance { get; set; }
 
-        [BsonElement("createdAt")]
-        public DateTime? CreatedAt { get; set; }
-
-        [BsonElement("updatedAt")]
-        public DateTime? UpdatedAt { get; set; }
-    }
-
-    [BsonIgnoreExtraElements]
-    public class RefinanceLoanStructureDTO
-    {
-        public Guid? Id { get; set; }
-
-        [BsonIgnore]
-        public Guid PreApprovalId { get; set; }
-
-        [BsonElement("loanProgram")]
-        public int LoanProgram { get; set; }
-
         [BsonElement("refinanceType")]
         public int RefinanceType { get; set; }  // 0 = Rate and Term, 1 = Cash Out
 
@@ -217,6 +199,36 @@ namespace LoanPortal.Core.Entities
 
         [BsonElement("mi")]
         public decimal? MI { get; set; }
+
+        [BsonElement("currentUnpaidBalance")]
+        public decimal? CurrentUnpaidBalance { get; set; }
+
+        [BsonElement("creditLimit")]
+        public decimal? CreditLimit { get; set; }
+
+        [BsonElement("currentDrawAmount")]
+        public decimal? CurrentDrawAmount { get; set; }
+
+        [BsonElement("createdAt")]
+        public DateTime? CreatedAt { get; set; }
+
+        [BsonElement("updatedAt")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+
+    [BsonIgnoreExtraElements]
+    public class RefinanceLoanStructureDTO
+    {
+        public Guid? Id { get; set; }
+
+        [BsonIgnore]
+        public Guid PreApprovalId { get; set; }
+
+        [BsonElement("loanProgram")]
+        public int LoanProgram { get; set; }
+
+        [BsonElement("lienPosition")]
+        public int? LienPosition { get; set; }
 
         [BsonElement("createdAt")]
         public DateTime? CreatedAt { get; set; }
--- LoanPortal.API/src/LoanPortal.Shared/Enum/Enums.cs
+++ LoanPortal.API/src/LoanPortal.Shared/Enum/Enums.cs
@@ -23,7 +23,10 @@ namespace LoanPortal.Shared.Enum
     {
         NonQM = 1,
         Conventional = 2,
-        FHA = 3
+        FHA = 3,
+        VA = 4,
+        HELOC = 5,
+        HELOAN = 6
     }
 
     public enum ApplicationStatus
@@ -47,9 +50,10 @@ namespace LoanPortal.Shared.Enum
         TwoUnit = 2,
         ThreeUnit = 3,
         FourUnit = 4,
-        Duplex = 5,
-        Triplex = 6,
-        FourPlex = 7
+        CondoTownhome = 5,
+        Duplex = 6,
+        Triplex = 7,
+        FourPlex = 8
     }
 
     public enum FormType

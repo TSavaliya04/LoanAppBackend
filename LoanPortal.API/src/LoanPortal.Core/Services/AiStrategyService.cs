7f0a0519047699a12e0b9ddab35ac09ad6bf3304
Tushar Savaliya <tusharsavaliya4444@gmail.com>
2026-09-09 22:24:14 +0530

    DTO restructuring minor change

--- LoanPortal.API/src/LoanPortal.Core/Services/AiStrategyService.cs
+++ LoanPortal.API/src/LoanPortal.Core/Services/AiStrategyService.cs
@@ -96,7 +96,7 @@ namespace LoanPortal.Core.Services
                         FrontEndDTI = lp.FrontEndRatio ?? 0,
                         BackEndDTI = lp.BackEndRatio ?? 0,
                         PrincipalAndInterest = lp.PrincipalAndInterest ?? 0,
-                        PropertyTax = ri.MonthlyTaxAmount ?? 0,
+                        PropertyTax = lp.MonthlyPropertyTax ?? 0,
                         MortgageInsurance = lp.MortgageInsurance ?? 0,
                         HoaFee = lp.HoaFee ?? 0,
                         MonthlyTotal = lp.MonthlyTotal ?? 0
